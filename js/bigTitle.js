2d5cf1c1d586b84d50e030ba18d2b3e31388662c
KHERRAZ Mohammed Achraf <achraf.sena@gmail.com>
2025-09-07 20:22:12 +0200

    Réorganisation du titre principal dans le HTML avec ajout d'une nouvelle structure pour le texte. Mise à jour des styles CSS/SCSS pour gérer le débordement et amélioration des animations avec GSAP dans le JavaScript. Intégration de nouveaux éléments pour une meilleure expérience utilisateur.

--- js/bigTitle.js
+++ js/bigTitle.js
@@ -6,23 +6,30 @@ gsap.registerPlugin(SplitText, ScrollTrigger);
 
 const bigTitle = () => {
     const bigTitleContainer = document.querySelector('#big-title');
-    const bigTitle = document.querySelector('#big-title-text');
+    const bigTitle = document.querySelector('#big-title-text .title-content');
     const bigTitle2 = document.querySelector('#big-title-text-2'); 
     const creative = document.querySelector('#creative');
+    const stamp = document.querySelector('.stamp-animation');
+    const logo = document.querySelector('.logo h2');
+    const nav = document.querySelectorAll('.main-nav .nav-links li');
+
+
     const title_1 = new SplitText(bigTitle, { type: "chars,words", charsClass: "char-js", wordsClass: "word-js" });
     const title_2 = new SplitText(bigTitle2, { type: "chars,words", charsClass: "char-js", wordsClass: "word-js" });
     const title_3 = new SplitText(creative, { type: "chars,words", charsClass: "char-js", wordsClass: "word-js" });
+
 
     // Utiliser les mots pour l'animation des titres 1 et 2
     const words_1 = title_1.words;
     const words_2 = title_2.words;
     const chars_3 = title_3.chars;
 
+
     // Création d'une timeline GSAP avec ScrollTrigger
     const tl = gsap.timeline({
         scrollTrigger: {
             start: 'top 50%',
-            trigger: bigTitle,
+            trigger: bigTitleContainer,
         }
     });
 
@@ -42,7 +49,23 @@ const bigTitle = () => {
         ease: "elastic.out(0.5,0.3)",
         stagger: 0.06,
     }, "<+0.5");
-    
+    tl.from(stamp, {
+        duration: 2.2,
+        scale: 0,
+        ease: "elastic.out(0.8,0.3)",
+    }, "<+1");
+    tl.from(logo, {
+        duration: 1.8,
+        yPercent: -100,
+        ease: "expo.out",
+        stagger: 0.06,
+    }, "<+0.5");
+    tl.from(nav, {
+        duration: 1.8,
+        yPercent: -100,
+        ease: "expo.out",
+        stagger: 0.2,
+    }, "<+0.5");
 }
 
 export default bigTitle;
